6c0004f4585a94b92d1ae2545b9d076d88adf29e
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-22 08:57:33 +0900

    change: change font colors and styles

--- src/src/components/Hello.tsx
+++ src/src/components/Hello.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 const Hello = (): JSX.Element => {
   return (
     <View>
-      <Text>Hello, World!</Text>
+      <Text style={styles.text}>Hello, World!</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+})
 
 export default Hello
