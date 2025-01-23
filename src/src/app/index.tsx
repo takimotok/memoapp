7d9518a1cc8e1c91d262d937654cf2a47d55e7e1
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-24 08:10:31 +0900

    change: add raw memo list

--- src/src/app/index.tsx
+++ src/src/app/index.tsx
@@ -1,11 +1,53 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hellow World!</Text>
+    <View style={styles.container}>
+      <View>
+        <View />
+        <View>
+          <Text>Memo App</Text>
+          <Text>logout</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <Text>買い物リスト_A</Text>
+          <Text>2025-01-24 08:04</Text>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+        <View>
+          <Text>買い物リスト_B</Text>
+          <Text>2025-01-25 08:04</Text>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+        <View>
+          <Text>買い物リスト_C</Text>
+          <Text>2025-01-26 08:04</Text>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 export default Index
