13196a67f9898a302c53128f80360b55c5f18e4a
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-28 09:13:46 +0900

    change: apply styles to memoListItem

--- src/src/app/index.tsx
+++ src/src/app/index.tsx
@@ -10,27 +10,31 @@ const Index = (): JSX.Element => {
         </View>
       </View>
 
-      <View>
+      <View style={styles.memoListItem}>
         <View>
-          <Text>買い物リスト_A</Text>
-          <Text>2025-01-24 08:04</Text>
-          <View>
-            <Text>x</Text>
-          </View>
+          <Text style={styles.memoListTitle}>買い物リスト_A</Text>
+          <Text style={styles.memoListDate}>2025-01-24 08:04</Text>
         </View>
         <View>
-          <Text>買い物リスト_B</Text>
-          <Text>2025-01-25 08:04</Text>
-          <View>
-            <Text>x</Text>
-          </View>
+          <Text>x</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListTitle}>買い物リスト_A</Text>
+          <Text style={styles.memoListDate}>2025-01-24 08:04</Text>
         </View>
         <View>
-          <Text>買い物リスト_C</Text>
-          <Text>2025-01-26 08:04</Text>
-          <View>
-            <Text>x</Text>
-          </View>
+          <Text>x</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListTitle}>買い物リスト_A</Text>
+          <Text style={styles.memoListDate}>2025-01-24 08:04</Text>
+        </View>
+        <View>
+          <Text>x</Text>
         </View>
       </View>
 
@@ -65,6 +69,25 @@ const styles = StyleSheet.create({
     color: 'rgba(255, 255, 255, 0.7)',
     position: 'absolute',
     right: 16,
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoListTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListDate: {
+    color: '#848484',
+    fontSize: 12,
+    lineHeight: 32,
   },
 })
 
