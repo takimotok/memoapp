3f6b3aa0779e60959ee63fc6628ccd3e13674250
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-02-13 17:44:03 +0900

    change: add expo-font, vector-icons and babel-plugin-module-resolver

--- src/babel.config.cjs
+++ src/babel.config.cjs
@@ -2,5 +2,28 @@ module.exports = api => {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.android.js',
+            '.android.jsx',
+            '.ios.js',
+            '.ios.jsx',
+            '.js',
+            '.json',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.ttf',
+          ],
+          // root: ['.'],
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
   }
 }
